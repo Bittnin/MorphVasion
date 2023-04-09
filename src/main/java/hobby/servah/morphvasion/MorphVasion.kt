c97a75960b94f4920cd2962b77d8c3b6fc5a63c5
Bittnin <bittninyt@gmail.com>
2023-04-09 18:00:23 +0200

    Initial Commit

---
+++ src/main/java/hobby/servah/morphvasion/MorphVasion.kt
@@ -0,0 +1,13 @@
+package hobby.servah.morphvasion
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MorphVasion : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
